d0738dffd267c6d50f4b034abbe6c086d43a4e06
ParasSidd <142650765+ParasSidd@users.noreply.github.com>
2024-09-30 18:26:20 +0530

    Basic backend with MongoDb connection completed

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then( () => {
+        console.log('MongoDB is connected');
+    }).catch(err => {
+        console.log(err);
+    });
+
+app.listen(3000, () => {
+    console.log('Server is running on 3000 port...');
+});
