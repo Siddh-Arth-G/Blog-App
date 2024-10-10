ab18b5f5daeefe716326134b64dfb61a5c3b5e9e
ParasSidd <142650765+ParasSidd@users.noreply.github.com>
2024-10-10 14:42:55 +0530

    Footer section Ui completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,12 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
