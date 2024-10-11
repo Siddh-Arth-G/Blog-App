b159c2e5d808f8c061692d16ba039a02a86e6c46
ParasSidd <142650765+ParasSidd@users.noreply.github.com>
2024-10-11 15:48:34 +0530

    Header section updation after sign in or sign up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -63,7 +63,7 @@ export const google = async(req, res, next) => {
     try{
         const user = await User.findOne({email});
         if(user){
-            const token = jwt.sign({id: user._id}, proces.env.JWT_SECRET);
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
             const {password, ...rest} = user._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
